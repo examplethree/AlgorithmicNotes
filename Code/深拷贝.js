b431c581b03024e2be49ea32f90c39d9836347b4
examplethree <shanty.doubles@gmail.com>
2024-01-05 22:39:50 +0800

    0105

--- Code/深拷贝.js
+++ Code/深拷贝.js
@@ -1,15 +1,12 @@
 // 深拷贝
-function deepClone(source){
-    let copy = Array.isArray(source)? [] : {};
-    if (source){
-        for( key in source){
-            if(source[key] !== null && Object.prototype.toString.call(source[key]) === '[object Object]'){
-                copy[key] = deepClone(source[key]);
-            }
-            else{
-                copy[key] = source[key];
-            }
-        }
+function deepClone2(source){
+    if(typeof source === 'object' && source){
+        const res = Array.isArray(source)? []:{};
+        const keys = Object.keys(source);
+        keys.forEach(key =>{
+            res[key] = deepClone2(source[key]);
+        })
+        return res;
     }
     return source;
 }
