f5f23058015054493fa2427bd08ef73a8944ac8c
examplethree <shanty.doubles@gmail.com>
2024-01-08 22:45:35 +0800

    0108

--- Code/深拷贝.js
+++ Code/深拷贝.js
@@ -1,11 +1,26 @@
 // 深拷贝
 function deepClone2(source){
-    if(typeof source === 'object' && source){
+    let dataType = Object.prototype.toString.call(source).slice(8, -1);
+    if(source && (dataType === 'Object' || dataType === 'Array')){
         const res = Array.isArray(source)? []:{};
         const keys = Object.keys(source);
         keys.forEach(key =>{
             res[key] = deepClone2(source[key]);
         })
+        return res;
+    }
+    return source;
+}
+
+// 浅拷贝
+function copy(source){
+    let dataType = Object.prototype.toString.call(source).slice(8, -1);
+    if(source && (dataType === 'Object' || dataType === 'Array')){
+        let res = Array.isArray(source)? []: {};
+        let keys = Object.keys(source);
+        for (key of keys){
+            res[key] = source[key];
+        }
         return res;
     }
     return source;
