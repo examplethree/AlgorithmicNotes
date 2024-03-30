7ffa0a8b611514bfe9f31595c90d1c39342d9bd0
shantyhub <51940411+examplethree@users.noreply.github.com>
2024-03-30 22:00:13 +0800

    小改动

---
+++ Code/防抖和节流的使用示例.js
@@ -0,0 +1,71 @@
+//HTML文件省略
+const input = document.querySelector("input");
+const defaultText = document.getElementById("default");
+const debounceText = document.getElementById("debounce");
+const throttleText = document.getElementById("throttle");
+
+
+const updatDebounceText = debounce(() => {
+  // debounceText.textContent = text;
+  incrementCount(debounceText);
+}, 250)
+
+const updatThrottleText = throttle(() => {
+  // throttleText.textContent = text;
+  incrementCount(throttleText);
+})
+
+// 输入框模拟
+// input.addEventListener("input", e => {
+//   defaultText.textContent = e.target.value;
+//   updatDebounceText(e.target.value);
+//   updatThrottleText(e.target.value);
+// })
+
+function debounce(cb, delay = 1000){
+  let timeoutId;
+
+  return (...arg) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() =>{
+      cb(...arg)
+    }, delay);
+  }
+}
+
+function throttle(cb, delay=1000){
+  let shouldWait = false;
+  let waitingArgs = null;
+
+  const timeoutFunc = function(){
+    if(waitingArgs !== null){
+      cb(...waitingArgs);
+      waitingArgs = null;
+      setTimeout(timeoutFunc, delay);
+    }
+    else{
+      shouldWait = false;
+    }
+  }
+
+  return (...arg) => {
+    if(shouldWait){
+      waitingArgs = arg;
+      return;   
+    }
+    cb(...arg);
+    shouldWait = true;
+    setTimeout(timeoutFunc, delay)
+  }
+}
+
+// 利用鼠标移动模拟
+document.addEventListener("mousemove", e => {
+  incrementCount(defaultText);
+  updatDebounceText();
+  updatThrottleText();
+})
+
+function incrementCount(element) {
+  element.textContent = (parseInt(element.innerText) || 0) + 1;
+}
